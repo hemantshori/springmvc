8b40a97c9b755eb506464e7f594c00c8a488a48f
hemant shori <hemantshori@gmail.com>
2018-05-25 16:02:18 +1000

    adding project

---
+++ src/main/java/mvc/LoginServlet.java
@@ -0,0 +1,41 @@
+package mvc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/views/Login.jsp").forward(request, response);
+		
+	}
+	
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	
+		UserValidationService validate = new UserValidationService();
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		 Boolean isUserValid = validate.validateUser(name,password);
+		
+		if(isUserValid)
+		{
+			
+			request.setAttribute("name", name);
+
+			request.setAttribute("password", password);
+			request.getRequestDispatcher("/WEB-INF/views/Welcome.jsp").forward(request, response);
+		}else{
+			request.getRequestDispatcher("/WEB-INF/views/Login.jsp").forward(request, response);
+		}
+		
+	}
+
+}
---
+++ src/main/java/mvc/UserValidationService.java
@@ -0,0 +1,15 @@
+package mvc;
+
+public class UserValidationService {
+
+	
+	public Boolean validateUser(String userName, String password)
+	{
+		if(userName.equals("hemant")&& password.equals("hemant"))
+		
+			return true;
+
+	return false;
+}
+	
+}
